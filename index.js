4778b0584f8dc7368bac97908769bd833481b70d
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-04-15 08:57:11 +0100

    my first commit

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ const knex = require('knex')(config.development);
 
 (async() => {
     try{
-        const version = await knex.raw('SELECT VERSION()');
-        console.log(version);
+        const employee = {
+            employee_id: 'LU-0001',
+            first_name: 'Ayooluwa',
+            last_name: 'Oyewo',
+            gender: 'male',
+            department: 'Software Engineering',
+            email: 'ayooluwaoyewo@gmail.com',
+            dob: '2019-04-06',
+            phone: '+2347030360670'
+        };
+        const result = await knex('employees').insert(employee);
+        console.log(result);
         
     }
     catch(error){
-        console.log(error)
+        console.log(error);
+        process.exit(1);
     }
 })();
--- migrations/20190415054851_create_employees_table.js
+++ migrations/20190415054851_create_employees_table.js
@@ -1,8 +1,37 @@
 
-exports.up = function(knex, Promise) {
-  
+exports.up = async (knex, Promise) => {
+    try{
+        const tableExists = await knex.schema.hasTable('employees');
+        
+        if(!tableExists){
+            return knex.schema.createTable('employees', (table) => {
+                table.increments();
+                table.string('employee_id', 100).notNullable();
+                table.string('first_name').notNullable();
+                table.string('middle_name').nullable();
+                table.string('last_name').notNullable();
+                table.enum('gender', ['male', 'female', 'binary', 'undefined', 'n/a']);
+                table.date('dob').nullable();
+                table.string('phone', 15).notNullable();
+                table.string('email', 100).nullable();
+                table.string('department', 250);
+
+                table.index('employee_id');
+                table.index('first_name');
+                table.index('last_name');
+                table.unique('email');
+                table.unique('employee_id');
+            });
+        } else {
+            console.log('Table already exists');
+            process.exit(1);
+        }
+    } catch(error){
+        console.log(error);
+    }
+    
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = async (knex, Promise) => {
+  return knex.schema.dropTable('employees');
 };
